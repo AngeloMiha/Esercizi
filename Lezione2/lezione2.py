9461f9ca156f3bfe6f74a0e31c5ca27e9d48b075
AngeloMiha <mihalacheangelo099@gmail.com>
2024-04-19 15:44:49 +0200

    Commit 3-6

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -26,7 +26,8 @@ fileNameCorrect = fileName.removesuffix(".txt")
 print(fileNameCorrect)
 
 # 3-1. Names
-print("\n Number 3-1.")
+print("\n")
+print("Number 3-1.")
 names = ["Gianni", "Mario", "Flavio", "Andrea", "Matteo"]
 friend1 = names[0]
 friend2 = names[1]
@@ -37,39 +38,54 @@ listFriends = [friend1, friend2, friend3, friend4, friend5] #questa lista è sta
 print (listFriends)
 
 # 3-2. Greetings
-print("\n Number 3-2.")
+print("\n")
+print("Number 3-2.")
 for name in names:
     print ("Grazie mille per aver partecipato all'attività", name )
 
 # 3-3. Your own list
-print("\n Number 3-3.")
+print("\n")
+print("Number 3-3.")
 vehicles = ["an S2000", "an s13", "an honda civic", "a mitsubishi evo", "a mitsubishi lancer", "an harley davinson"]
 for vehicle in vehicles:
     print("Surely", vehicle + " is in my dream garage")
 
 # 3-4. Guest List
-print("\n Number 3-4.")
-guests = ["Travis Scott", "Ye", "mamma", "Mattia Ro'", "Cerratti", "Cuozzo"]
+print("\n")
+print("Number 3-4.")
+guests = ["Travis Scott", "Ye", "mamma", "Mattia Rò", "Cerratti", "Cuozzo"]
 for guest in guests:
-    print("Hey", guest + " want to join us or dinner?")
+    print("Hey", guest + ", want to join us or dinner?")
 
 # 3-5. Changing Guest List
-print("\n Number 3-5.")
-guests = ["Travis Scott", "Ye", "mamma", "Mattia Ro'", "Cerratti", "Cuozzo"]
-for guest in guests:
-    print("Hey", guest + " , want to join us or dinner?")
 print("\n")
+print("Number 3-5.")
+for guest in guests:
+    print("Hey", guest + ", want to join us or dinner?")
 CMIguest = random.choice(guests)
-print("Tonight", CMIguest, " can't make to the dinner sadly :(")
+print("\nTonight", CMIguest, " can't make to the dinner sadly :(")
 guests.remove(CMIguest)
-new_guest = "papa'"
+new_guest = "papà"
 guests.append(new_guest)
-print("\n")
-for guest in guests:
-    print("Hey", guest + " , want to join us or dinner?")
+print("\nHey", new_guest + ", want to join us or dinner?")
 
 
 # 3-6. More Guests
+print("\n")
+print("Number 3-6.")
+for guest in guests:
+    print("Hey", guest + ", want to join us or dinner?")
+new_guest_beginning = "nonna"
+guests.insert(0, new_guest_beginning)
+new_guest_middle = "nonno"
+middle_index = len(guests) // 2
+guests.insert(middle_index, new_guest_middle)
+new_guest_end = "sorella"
+guests.append(new_guest_end)
+print("\nUpdated guest list:")
+for guest in guests:
+    print("Hey", guest + ", want to join us for dinner?")
+print("\nGood news! We found a bigger dinner table.")
 
 
 # 3-7. Shrinking Guest List:
