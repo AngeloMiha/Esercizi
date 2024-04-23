82368943dd5173506651ba27649ac5d866130acf
MihaAngelo <mihalacheangelo099@gmail.c>
2024-04-23 11:06:26 +0200

    Commit number 4-2

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -5,11 +5,28 @@ i: int = 1
 for pizza in pizzas:
     print("Pizza number", i, "is", pizza)
     i += 1
-
 i = 0
 print("I really like", pizzas[i], "pizza !!! ")
 i += 1
 print("I really like", pizzas[i], "pizza !!! ")
 i += 1
 print("I really like", pizzas[i], "pizza !!! ")
+
+
+# Number 4 - 2
+print("\n")
+print("Number 4-2:")
+animals: list[str] = ["monkey", "tiger", "cat"]
+i: int = 1
+for animal in animals:
+    print("Animal number", i, "is", animal)
+    i += 1
+i = 0
+print("The", animals[i], "is my faourite animal")
+i += 1
+print("The", animals[i], "is the king of the jungle")
+i += 1
+print("I'm allergic to a", animals[i])
+print("All this animals are my favourite !!")
+
 
