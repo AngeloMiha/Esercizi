16938ebd705b975f967b3889bd39bc206edbdc7d
MihaAngelo <mihalacheangelo099@gmail.c>
2024-04-23 10:57:45 +0200

    Commit number 4-1

---
+++ Lezione3/lezione3.py
@@ -0,0 +1,15 @@
+# Number 4 - 1
+print("Number 4-1:")
+pizzas: list[str] = ["margherita", "crostino", "diavola"]
+i: int = 1
+for pizza in pizzas:
+    print("Pizza number", i, "is", pizza)
+    i += 1
+
+i = 0
+print("I really like", pizzas[i], "pizza !!! ")
+i += 1
+print("I really like", pizzas[i], "pizza !!! ")
+i += 1
+print("I really like", pizzas[i], "pizza !!! ")
+
