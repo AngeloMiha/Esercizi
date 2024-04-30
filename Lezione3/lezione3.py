d1a7ebdde58f5d29c05b62e788bee13d2ce8877f
MihaAngelo <mihalacheangelo099@gmail.com>
2024-04-30 17:29:09 +0200

    Commit Number 5-6

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -227,7 +227,20 @@ else:
 # 5-6. Stages of Life
 print("\n")
 print("Number 5-6:")
+age = 25
 
+if age < 2:
+    print("The person is a baby.")
+elif age < 4:
+    print("The person is a toddler.")
+elif age < 13:
+    print("The person is a kid.")
+elif age < 20:
+    print("The person is a teenager.")
+elif age < 65:
+    print("The person is an adult.")
+else:
+    print("The person is an elder.")
 
 
 # 5-7. Favorite Fruit
