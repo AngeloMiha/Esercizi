448a86189d6fab26ef7052cef8d5f3b518832683
MihaAngelo <mihalacheangelo099@gmail.c>
2024-05-02 16:35:36 +0200

    Commit Numero Magico

--- Lezione5/Lezione5.py
+++ Lezione5/Lezione5.py
@@ -1,5 +1,4 @@
 # Scrivi una funzione che calcola la media di una lista di numeri e ritorna il valore arrotondato all'intero più vicino.
-
 def rounded_average(numbers: list[int]) -> int:
     somma: int = 0
     med: int = 0
@@ -9,4 +8,19 @@ def rounded_average(numbers: list[int]) -> int:
         cont += 1
     med = somma / cont
     return round(med)
-print(rounded_average([1, 1, 2, 2]))
+
+
+# Scrivi una funzione che, dato un numero intero, determina se è un "numero magico".
+# Un numero magico è definito come un numero che contiene il numero 7.
+def is_magic_number(num: int) -> bool:
+    lista: list[int] = []
+    for num in str(num):
+        lista.append(num)
+    
+    for number in lista:
+        if number == '7':
+            return True
+        else:
+            continue
+    return False
+
