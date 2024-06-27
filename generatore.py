c50bd55ab3ee4a2852f9e7ff6e6db56973d59ae5
MihaAngelo <mihalacheangelo099@gmail.com>
2024-06-27 18:54:12 +0200

    boh sus

---
+++ generatore.py
@@ -0,0 +1,11 @@
+def generatore():
+
+    yield "A"
+    yield "B"
+    yield "C"
+
+prova_generatore = generatore()
+
+print(next(prova_generatore))
+print(next(prova_generatore))
+print(next(prova_generatore))
